420def0c46db09ebf4452334c5bc596d9347fc37
Anthony Oliveri <oliveri@us.ibm.com>
2016-03-21 15:16:21 -0500

    New Cocoa Touch Framework project

---
+++ BMSAnalyticsSpec/BMSAnalyticsSpec/BMSAnalyticsSpec.h
@@ -0,0 +1,19 @@
+//
+//  BMSAnalyticsSpec.h
+//  BMSAnalyticsSpec
+//
+//  Created by Anthony Oliveri on 3/21/16.
+//  Copyright © 2016 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMSAnalyticsSpec.
+FOUNDATION_EXPORT double BMSAnalyticsSpecVersionNumber;
+
+//! Project version string for BMSAnalyticsSpec.
+FOUNDATION_EXPORT const unsigned char BMSAnalyticsSpecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMSAnalyticsSpec/PublicHeader.h>
+
+
